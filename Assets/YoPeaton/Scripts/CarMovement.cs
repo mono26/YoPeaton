b28a3cdd1bf578874e6789b0f6295e2ec71b2845
Desktop-Mono <nzubiria1@gmail.com>
2019-10-03 20:06:25 -0500

    Unity project initialized

---
+++ Assets/YoPeaton/Scripts/CarMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D carBody = null;
+    [SerializeField]
+    private float maxSpeed = 0.0f;
+    [SerializeField]
+    private float acceleration = 0.0f;
+}
