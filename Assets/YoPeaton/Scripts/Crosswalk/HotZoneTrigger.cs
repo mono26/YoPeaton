c299884823ecdeb8f0fb48ea02fe51e3a97d6281
NicolasAJ95 <nicolas.ajaramillo@live.com>
2019-10-23 22:58:23 -0500

    reverse of lost scripts

---
+++ Assets/YoPeaton/Scripts/Crosswalk/HotZoneTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotZoneTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Crosswalk owner;
+
+    [SerializeField]
+    public int objectsInTriggerCount = 0;
+
+    public Crosswalk GetOwner {
+        get {
+            return owner;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D _other) {
+        objectsInTriggerCount++;
+        if (_other.CompareTag("Pedestrian") || _other.CompareTag("Car")) {
+            owner?.OnEntering(_other.GetComponent<EntityController>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _other) {
+        objectsInTriggerCount--;
+        if (_other.CompareTag("Pedestrian") || _other.CompareTag("Car")) {
+            owner?.OnExited(_other.GetComponent<EntityController>());
+        }
+    }
+}
