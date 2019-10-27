62103b969f19ccba4e71e768b79bf4a5f0018bf0
NicolasAJ95 <nicolas.ajaramillo@live.com>
2019-10-27 17:33:14 -0500

    -Animator controller - animations integrated

---
+++ Assets/YoPeaton/Scripts/Controllers/AnimatorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    public Animator Animator
+    {
+        get
+        {
+            return animator;
+        }
+        set
+        {
+            animator = value;
+        }
+    }
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    public void SetCurrentAnimation(Directions direction)
+    {
+        switch (direction)
+        {
+            case Directions.North:
+                animator.SetBool("Front", true);
+                break;
+
+            case Directions.South:
+                animator.SetBool("Back", true);
+                break;
+
+            case Directions.East:
+                animator.SetBool("Right", true);
+                break;
+
+            case Directions.West:
+                animator.SetBool("Left", true);
+                break;
+        }
+    }
+
+}
